7f6e7f279766c92c0b0ab396b8c9afb2e0e62928
agent725 <agent725@725.be>
2017-12-06 22:41:53 +0100

    support for Ark cryptocurrency

---
+++ modules/deterministic/ark/deterministic.js
@@ -0,0 +1,36 @@
+// (C) 2015 Internet of Coins / Metasync / Joachim de Koning
+// hybridd module - ark/deterministic.js
+// Deterministic encryption wrapper for Ark
+//
+// [!] Browserify this and save to deterministic.js.lzma to enable sending it from hybridd to the browser!
+//
+
+var wrapper = (
+	function() {
+
+		var functions = {
+			// create deterministic public and private keys based on a seed
+			keys : function(data) {
+				// return object { publicKey:'', privateKey:'' }
+				return wrapperlib.crypto.getKeys(data.seed);
+			},
+
+      // generate a unique wallet address from a given public key
+      address : function(data) {
+        return wrapperlib.crypto.getAddress(data.publicKey);
+      },
+
+			transaction : function(data) {
+        // return deterministic transaction data
+				return JSON.stringify( wrapperlib.transaction.createTransaction(data.target, parseInt(data.amount), data.seed) );
+        // for more information see: https://github.com/corsaro1/lisk_broadcast
+			},
+
+		}
+
+		return functions;
+	}
+)();
+
+// export the functionality to a pre-prepared var
+deterministic = wrapper;
