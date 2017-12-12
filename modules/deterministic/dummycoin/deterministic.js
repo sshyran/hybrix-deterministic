5099af6aa0641c977590b526d0c5ae89721065cd
agent725 <agent725@725.be>
2017-12-12 18:53:46 +0100

    dummycoin for testing transactions

---
+++ modules/deterministic/dummycoin/deterministic.js
@@ -0,0 +1,32 @@
+// (C) 2015 Internet of Coins / Metasync / Joachim de Koning
+// hybridd module - dummycoin/deterministic.js
+// Deterministic encryption wrapper for Dummy test coin
+//
+// [!] Browserify this and save to deterministic.js.lzma to enable sending it from hybridd to the browser!
+//
+
+var wrapper = (
+  function() {
+
+    var functions = {         
+      // create deterministic public and private keys based on a seed
+      keys : function(data) {
+        return { dummy:'dummy' };
+      },
+
+      // generate a unique wallet address from a given public key
+      address : function(data) {
+        return '_dummyaddress_';
+      },
+
+      transaction : function(data) {
+        return '_dummytransaction_';
+      },
+    }
+
+    return functions;
+  }
+)();
+
+// export the functionality to a pre-prepared var
+deterministic = wrapper;
