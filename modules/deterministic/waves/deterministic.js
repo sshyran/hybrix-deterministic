9f51d07ab6feb896f0bc22769e6333a7bba7bfa9
Rouke Pouw <34579399+RoukePouw@users.noreply.github.com>
2018-03-13 14:49:51 +0100

    Add token support for waves

--- modules/deterministic/waves/deterministic.js
+++ modules/deterministic/waves/deterministic.js
@@ -19,12 +19,10 @@ var wrapper = (
       },
 
       transaction: function(data,callback) {
+        var txParams;
 
-        if (data.mode != 'token') {
-          // set the parameters
-
-          var txParams = { // optional-> data: payloadData
-
+        if (data.mode !== 'token') {
+          txParams = {
             recipient: data.target,
             // ID of a token, or WAVES
             assetId: 'WAVES',
@@ -32,32 +30,33 @@ var wrapper = (
             amount: parseInt(data.amount),
             // The same rules for these two fields
             feeAssetId: 'WAVES',
-            fee: 100000,
+            fee: parseInt(data.fee),
             // 140 bytes of data (it's allowed to use Uint8Array here)
-            attachment: '',
+            attachment: '', //FUTURE: add a message?
             timestamp: Date.now()
           };
+
         } else {
-        /*  // TEST: var encoded = encode({'func':'balanceOf(address):(uint256)','vars':['target'],'target':data.target});
-          var encoded = encode({
-            'func': 'transfer(address,uint256):(bool)',
-            'vars': ['target', 'amount'],
-            'target': data.target,
-            'fee_to_pay': '100000',
-            'amount': parseInt(data.amount).toString(16)
-          }); // returns the encoded binary (as a Buffer) data to be
-          // set the parameters
-          var txParams = {
-            //is nonce needed?
-            nonce: '0x' + parseInt(data.unspent.nonce).toString(16), // nonce
-            to: data.toAddr, // send payload to contract address
-            value: '0x' + parseInt(0).toString(16), // set to zero, since we're sending tokens
-            data: encoded // payload as encoded using the smart contract
-            };*/
+
+          txParams = {
+            recipient: data.target,
+            // ID of a token, or WAVES
+            assetId: data.contract,
+            // The real amount is the given number divided by 10^(precision of the token)
+            amount: parseInt(data.amount),
+            // The same rules for these two fields
+            feeAssetId: 'WAVES',
+            fee: parseInt(data.fee),
+            // 140 bytes of data (it's allowed to use Uint8Array here)
+            attachment: '', //FUTURE: add a message?
+            timestamp: Date.now()
+          };
         }
+
         Waves.API.Node.v1.assets.transfer(txParams, data.keys.keyPair).then((responseData) => {}).catch(function (error) {
           callback(error.data); // Since we've hacked the fetch command it will error, but we don't need the result, we need the request
         });
+
       }
     }
 
