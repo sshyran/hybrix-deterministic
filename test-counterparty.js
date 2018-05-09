4dbe7212439f5cd26f19bb321868cef8f2aa59b6
agent725 <agent725@725.be>
2018-05-09 18:27:21 +0200

    working test for counterparty transactions using BitcoinJSlib

--- test-counterparty.js
+++ test-counterparty.js
@@ -11,8 +11,7 @@ LZString = require('./lib/crypto/lz-string');
 Decimal = require('./lib/crypto/decimal-light'); Decimal.set({ precision: 64 });  // high precision for nonces
 //UrlBase64 = require('./crypto/urlbase64');
 hex2dec = require('./lib/crypto/hex2dec');
-
-  jsdom = require('jsdom');
+jsdom = require('jsdom');
 
 //
 // first some most often used global functions
@@ -42,6 +41,7 @@ toInt = function(input,factor) {
 // first we read from the compiled package and activate the code
 //
 
+//var mode = 'bitcore.livenet';  // other modes: bitcoinjslib.bitcoin, ethereum, lisk
 var mode = 'bitcoinjslib.counterparty';  // other modes: bitcoinjslib.bitcoin, ethereum, lisk
 dcode = String(fs.readFileSync('./modules/deterministic/'+mode.split('.')[0]+'/deterministic.js.lzma'))
 //require(LZString.decompressFromEncodedURIComponent(dcode));
@@ -52,21 +52,27 @@ var input = {}
 var tx = {
       'bitcoinjslib.counterparty': {
         'seed':'correct horse battery staple',                  // seed string for deterministic wallet
-        'symbol': "xcp",
         'keys':null,                                            // cryptographic keys (will be generated)
         'source_address':null,                                  // where to transact from (will be generated)
-        'target_address':'1ATDudWEEZmB2TKcEehZwUX2i3wBxQrSDK', // where to transact to
-        'contract':'',                                          // TODO? -> smart contract address
-        'amount':0.005,                                           // amount to send
-        'fee':0.001,                                            // fee for the miners or the system
+        'target_address':'19afYX8D5G8nGVJte6z7png1gfeUaHdXid',  // where to transact to
+        'contract':'XCP',                                       // TODO? -> smart contract address
+        'amount':"0.01",                                          // amount to send
+        'fee':"0.00002",                                          // fee for the miners or the system
         'unspent':                                              // Bitcoin derived cryptocurrencies need unspents to be able to generate transactions
             {
-              "payload":"434e54525052545900000000000000000000000100038d7ea4c68000", //  (0.1 XCP)
-              "unspents":[{"amount":"0.00007800","txid":"aa3121aadee3ae9412aaf3c23d1368deaa27083eecc2ddc848cd75d9503d1a9a","txn":0},{"amount":"0.73990000","txid":"fc7acd529e590002ebca9c7430cab4fa42d33b61f404a40f548e754b72d33962","txn":36}],"change":"0.1"
+              "unspents":[{"script":"76a914b31e2e79c5bdf335cc7be3e22c948d5c5f3ba0ff88ac","amount":"0.00033920","txid":"fd6a4dcc29ad5cbd2d3ccddea2788ef80828c3b28cea6b20bf603782ebbc05c2","txn":1}],"change":"0.0003042",
+              "unsignedtx":"0100000001c205bceb823760bf206bea8cb2c32808f88e78a2decd3c2dbd5cad29cc4d6afd010000001976a914b31e2e79c5bdf335cc7be3e22c948d5c5f3ba0ff88acffffffff020000000000000000306a2e3ffaa8ac4c1f16f65f8f2ff3f75f66453d620485b2ddbbedf9e933254d7c0b576d8124f621ddaa49987972f87c95d4760000000000001976a914b31e2e79c5bdf335cc7be3e22c948d5c5f3ba0ff88ac00000000"
             },
-        'factor':8                                            // amount of decimals, i.e.: 10^x
+        'factor':8,                                             // amount of decimals, i.e.: 10^x
       }
     }
+
+/*
+
+unsigned TX for: /asset/xcp/unspent/1HL67PWtvZaQfDoxuGXivn8U5WJ76toceZ/0.01/1KWsPEb3bp7nQuodUtZYHQsXmBCDZ9dtPz
+0100000001b84fcb6bca5075bb753c8100e835805703f536a7ac27b3a5f6de45c5a7d65ffd000000001976a914b31e2e79c5bdf335cc7be3e22c948d5c5f3ba0ff88acffffffff020000000000000000306a2ea37397180cd2c4e9aa24c87ae95e04fc1cbf24857a88b0433d84d22529259deccc37fa73a27775e737cce631787cf4240f00000000001976a914b31e2e79c5bdf335cc7be3e22c948d5c5f3ba0ff88ac00000000
+
+ */
 
 if(typeof deterministic!='object' || deterministic=={}) {
   logger('Error: Cannot load deterministic wrapper!');
@@ -100,13 +106,13 @@ if(typeof deterministic!='object' || deterministic=={}) {
   tx[mode].unspent.change = toInt(tx[mode].unspent.change,tx[mode].factor);
 
   // amend fee to account for payload bytes (Counterparty)
-  tx[mode].fee = tx[mode].fee + (0.0000025*tx[mode].unspent.payload.length);
-  logger('FEE AFTER PAYLOAD: '+tx[mode].fee+' BTC');
+  //tx[mode].fee = tx[mode].fee + (0.0000025000*tx[mode].testpayload.length);
+  //logger('FEE AFTER PAYLOAD: '+tx[mode].fee+' BTC');
 
   // put it all together
   input = {
             mode:mode.split('.')[1],
-            source:tx[mode].source_address,
+            source:'1HL67PWtvZaQfDoxuGXivn8U5WJ76toceZ',//tx[mode].source_address,
             target:tx[mode].target_address,
             amount:toInt(tx[mode].amount,tx[mode].factor),
             fee:toInt(tx[mode].fee,tx[mode].factor),
