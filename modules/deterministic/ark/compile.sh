4ae145935f707a1a6044f73acdc71d3c8fe5068e
Rouke Pouw <34579399+RoukePouw@users.noreply.github.com>
2018-07-24 16:31:53 +0200

    Fix Ark missing dependency and add webpack compiling

--- modules/deterministic/ark/compile.sh
+++ modules/deterministic/ark/compile.sh
@@ -4,16 +4,12 @@ WHEREAMI=`pwd`
 export PATH=$WHEREAMI/../../../node/bin:"$PATH"
 NODEINST=`which node`
 
-
-#cp ./ark-js ./wrapperlib.js
-
-#../../../node_modules/webpack/bin/webpack.js --config webpack.config.js
-#../../../tools/lzmapack.js bundle.js
-#mv bundle.js.lzma deterministic.js.lzma
+../../../node_modules/webpack/bin/webpack.js --config webpack.config.js
+../../../tools/lzmapack.js bundle.js
+mv bundle.js.lzma deterministic.js.lzma
 
 # clean up
-#rm bundle.js
-#rm wrapperlib.js
+rm bundle.js
 
 # restore path
 PATH=$OLDPATH
