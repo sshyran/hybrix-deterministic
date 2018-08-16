4ae50b89861214508c0a640a36ad60028ea033ad
“Marco <marco.alkema@gmail.com>
2018-08-16 15:11:39 +0200

    Update webpack config.

--- modules/deterministic/ethereum/webpack.config.js
+++ modules/deterministic/ethereum/webpack.config.js
@@ -6,19 +6,5 @@ module.exports = {
     path: path.resolve(__dirname, '.'),
     filename: 'bundle.js'
     ,library: 'test' // added to create a library file
-  },
-  // module: {
-  //   rules: [
-  //     {
-  //       exclude: path.resolve(__dirname, "../../../node_modules"),
-  //       test: /\.js$/,
-  //       use: {
-  //         loader: 'babel-loader',
-  //         options: {
-  //           presets: ['es2015']
-  //         }
-  //       }
-  //     }
-  //   ]
-  // }
+  }
 };
