0faeec93efc4ef81806464da17218ac4c50f7206
agent725 <agent725@725.be>
2018-10-25 17:13:59 +0200

    Dash now using uncompiled library

---
+++ modules/dash/wrapperlib.js
@@ -0,0 +1,11 @@
+//
+// wrapperlib to include libraries for incorporation into the virtual DOM
+//
+
+
+// inclusion of necessary requires
+var dashcorelib = {
+  dashcore : require('./dashcore-lib')
+}
+
+module.exports = dashcorelib;
