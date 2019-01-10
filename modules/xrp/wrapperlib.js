2be7cebb8a8f3afd7b4651ba96e003ded3be2910
jacobpetrovic85 <jacobpetrovic85@gmail.com>
2019-01-10 14:41:18 +0100

    first XRP commit

---
+++ modules/xrp/wrapperlib.js
@@ -0,0 +1,47 @@
+//
+// wrapperlib to include libraries for incorporation into the virtual DOM
+//
+
+
+
+// inclusion of necessary requires
+var dummylib = {
+
+    // create deterministic public and private keys based on a seed
+    keys : function(data) {
+      return { public:'_dummypublickey_', private: '_dummyprivatekey_'};
+    },
+
+  // generate a unique wallet address from a given public key
+  address : function(data) {
+    if(typeof data === 'object' && data.public === '_dummypublickey_' && data.private === '_dummyprivatekey_'){
+      return '_dummyaddress_';
+    }else{
+      throw('Expected correct keys.');
+    }
+  },
+
+  publickey : function(data) {
+    if(typeof data === 'object' && data.public === '_dummypublickey_' && data.private === '_dummyprivatekey_'){
+      return '_dummypublickey_';
+    }else{
+      throw('Expected correct keys.');
+    }
+  },
+
+  privatekey : function(data) {
+    if(typeof data === 'object' && data.public === '_dummypublickey_' && data.private === '_dummyprivatekey_'){
+      return '_dummyprivatekey_';
+    }else{
+      throw('Expected correct keys.');
+    }
+  },
+
+  transaction : function(data, callback) {
+    callback('_dummytransaction_'+data.amount+'_'+data.fee);
+  }
+
+//  dummyfy : require('./bitcore-lib'),
+}
+
+module.exports = dummylib;
