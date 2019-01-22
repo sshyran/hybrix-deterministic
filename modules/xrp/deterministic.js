aeeb7fd40f4990582e0f5211f867debb87002cdc
jacobpetrovic85 <jacobpetrovic85@gmail.com>
2019-01-22 10:23:11 +0100

    so far so good

--- modules/xrp/deterministic.js
+++ modules/xrp/deterministic.js
@@ -9,36 +9,17 @@ const rippleKeyPairs = require("ripple-keypairs");
 // const apiFactory = require('../');
 const apiFactory = require('x-address-codec');
 const createHash = require('create-hash');
-const hex2base32 = require('./../../common/crypto/hex2base32.js');
 const api = new RippleAPI();
 var BASE58 = '123456789ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 var bs58 = require('base-x')(BASE58);
-GL.ripple = api;
-GL.keyPairs = rippleKeyPairs;
-
-
-function makeStringTo16LengthNumber (acc, n, i) {
-    if (String(acc).length < 16) {
-      return acc + (n * (Math.pow(58, i+1)));
-    } else if (String(acc).length > 16) {
-      return Number(String(acc).slice(0, 31));
-    } else {
-      return acc; // === length 32
-    }
-}
-
-function makeIntoNumber (data) {
-  return data.seed
-    .split('')
-    .map(str => str.charCodeAt(0))
-    .reduce(makeStringTo16LengthNumber, 0);
-};
+// GL.ripple = api;
+// GL.keyPairs = rippleKeyPairs;
 
     //instantiate Ripple in offline mode for securing keysfrom the ripplenetwork
     var wrapper = {
       // create deterministic public and private keys based on a seed
       keys : data => {
-        console.log("data.seed = ", data.seed);
+        // console.log("data keys = ", data);
         var api2 = apiFactory({
           // We probably have your favorite alphabet, if not, contact us
           defaultAlphabet: 'ripple',
@@ -49,43 +30,32 @@ function makeIntoNumber (data) {
           // We'll endow your api with encode|decode* for you
           codecMethods : {
             // public keys
-            // AccountID : {version: 0x00},
+            AccountID : {version: 0x00},
             // secrets
             Seed: {version: 0x21}
           }
         });
-        const number = makeIntoNumber(data);
-        console.log("number = ", number);
-
-        var buf = Buffer.from(number.toString() + number.toString().split('').reverse().join(''), 'hex');
-
-        //'00000000000000000000000000000000'
-        console.log("buf = ", buf);
+        var hash = window.nacl.to_hex(nacl.crypto_hash_sha256(data.seed));
+        var secret = Buffer.from(hash.substr(0,32), 'hex');
         // It can encode a Buffer
-        var encoded = api2.encodeSeed(buf);
-        console.log("encoded = ", encoded);
+        var encoded = api2.encodeSeed(secret);
         return rippleKeyPairs.deriveKeypair(encoded);
       },
 
       // generate a unique wallet address from a given public key
       address : data => {
-        console.log("data = ", data);
-        console.log("data address = ", rippleKeyPairs.deriveAddress(data.publicKey));
+        // console.log("data address = ", data);
         return rippleKeyPairs.deriveAddress(data.publicKey);
       },
 
       // return public key
       publickey : data => {
-        console.log("data = ", data);
-        console.log("data  pubKey= ", data.publicKey);
         return data.publicKey;
       },
 
       // return private key
       privatekey : data => {
-        console.log("data = ", data);
-        console.log("data privKey = ", data.privateKey);
-        return data.privatekey;
+        return data.privateKey;
       },
 
       // generate a transaction
@@ -93,30 +63,5 @@ function makeIntoNumber (data) {
         return api.transaction(data,callback);
       }
     }
-
-// const RippleAPI = require('ripple-lib').RippleAPI;
-
-// const api = new RippleAPI({
-//   server: 'wss://s1.ripple.com' // Public rippled server hosted by Ripple, Inc.
-// });
-// api.on('error', (errorCode, errorMessage) => {
-//   console.log(errorCode + ': ' + errorMessage);
-// });
-// api.on('connected', () => {
-//   console.log('connected');
-// });
-// api.on('disconnected', (code) => {
-//   // code - [close code](https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent) sent by the server
-//   // will be 1000 if this was normal closure
-//   console.log('disconnected, code:', code);
-// });
-// api.connect().then(() => {
-//   /* insert code here */
-// }).then(() => {
-//   return api.disconnect();
-// }).catch(console.error);
-
-// export the functionality to a pre-prepared var
-//module.exports = wrapper;
 
 window.deterministic = wrapper;
