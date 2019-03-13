f405a9130d389696be35929c5d70c66ea5fe4938
Rouke Pouw <34579399+RoukePouw@users.noreply.github.com>
2019-03-13 16:00:09 +0100

    Update deglobalify.js

--- scripts/deglobalify/deglobalify.js
+++ scripts/deglobalify/deglobalify.js
@@ -10,25 +10,24 @@ var source = process.argv[2];
 var noUndef3 = process.argv[3];
 
 fs.readFile(noUndef3, 'utf8', function (err, unDefVars) {
-  if(err) throw err;
+  if (err) throw err;
   var array = unDefVars.toString().split('\n');
 
   fs.readFile(source, 'utf8', function (e, bundle) {
-    var vars = array
-        .filter(function (var_) {return var_ !== '' &&
-                                 var_ !== 'proto' && // For tron
-                                 var_ !== 'DataView' && // For eth
-                                 var_ !== 'nacl' && var_ !== 'naclInstance';  // For lsk
-                                })
+    var vars = array.filter(function (var_) {return var_ !== '' &&
+                                             var_ !== 'proto' && // For tron
+                                             var_ !== 'DataView' && // For eth
+                                             var_ !== 'nacl' && var_ !== 'naclInstance';})  // For lsk
+
         .reduce(function (acc, variable) {
           return window.hasOwnProperty(variable)
             ? acc + ''
             : acc + variable + ',';
         }, 'var ');
-    if (vars === 'var '){ //OMG no global variables, nothing to do
+    if (vars === 'var ') { //OMG no global variables, nothing to do
       process.stdout.write(bundle);
-    }else{
-      var finalCodeWithVars = vars.replace(/.$/, ";\ ") + '      ' + bundle;
+    } else {
+      var finalCodeWithVars = vars.replace(/.$/, '; ') + '      ' + bundle;
       process.stdout.write(finalCodeWithVars);
     }
   })
