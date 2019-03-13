7376e6c3a7c9ffd94f9112ece086adea746deada
Rouke Pouw <34579399+RoukePouw@users.noreply.github.com>
2019-03-13 16:02:05 +0100

    Update deglobalify.js

--- scripts/deglobalify/deglobalify.js
+++ scripts/deglobalify/deglobalify.js
@@ -18,13 +18,13 @@ fs.readFile(noUndef3, 'utf8', function (err, unDefVars) {
       return var_ !== '' &&
         var_ !== 'proto' && // For tron
         var_ !== 'DataView' && // For eth
-        var_ !== 'nacl' && var_ !== 'naclInstance';  // For lsk
+        var_ !== 'nacl' && var_ !== 'naclInstance'; // For lsk
     })
-        .reduce(function (acc, variable) {
-          return window.hasOwnProperty(variable)
-            ? acc + ''
-            : acc + variable + ',';
-        }, 'var ');
+      .reduce(function (acc, variable) {
+        return window.hasOwnProperty(variable)
+          ? acc + ''
+          : acc + variable + ',';
+      }, 'var ');
     if (vars === 'var ') { // OMG no global variables, nothing to do
       process.stdout.write(bundle);
     } else {
