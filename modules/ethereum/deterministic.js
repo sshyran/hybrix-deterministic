264360d08efea56c0117fa1e7209eea996c6d209
Rouke Pouw <34579399+RoukePouw@users.noreply.github.com>
2019-03-26 12:25:53 +0100

    Import keys for eth and dummy

--- modules/ethereum/deterministic.js
+++ modules/ethereum/deterministic.js
@@ -1,92 +1,95 @@
 // (C) 2017 Internet of Coins / Joachim de Koning
 // Deterministic encryption wrapper for Ethereum
 
-var wrapperlib = require('./wrapperlib');
+let wrapperlib = require('./wrapperlib');
 
-
-var wrapper = (
-  function() {
+let wrapper = (
+  function () {
     // encode ABI smart contract calls
     // call it by explicitly specifying the variables you want to pass along
     //
     // EXAMPLES:
     //            encode({ 'func':'balanceOf(address):(uint256)', 'vars':['target'], 'target':data.target });
     //            encode({ 'func':'transfer(address,uint256):(uint256)', 'vars':['target','amount'], 'target':data.target,'amount':parseLargeIntToHex(data.amount).toString('hex') });
-    function encode(data) {
-      return '0x'+( new Function( 'wrapperlib','data', 'return wrapperlib.ethABI.simpleEncode(data.func,data.'+data.vars.join(',data.')+');' ) )(wrapperlib,data).toString('hex');
+    function encode (data) {
+      return '0x' + (new Function('wrapperlib', 'data', 'return wrapperlib.ethABI.simpleEncode(data.func,data.' + data.vars.join(',data.') + ');'))(wrapperlib, data).toString('hex');
     }
 
-    function parseLargeIntToHex(input) {
-      var result = wrapperlib.hex2dec.toHex( new Decimal(String(input)).toInteger().toFixed(64).replace(/\.?0+$/,"") );
+    function parseLargeIntToHex (input) {
+      let result = wrapperlib.hex2dec.toHex(new Decimal(String(input)).toInteger().toFixed(64).replace(/\.?0+$/, ''));
       return result !== null ? result : '0x0';
       // DEPRECATED: return new Decimal(String(input)).toInteger().toFixed(64).replace(/\.?0+$/,"");
     }
 
-    var functions = {
+    let functions = {
 
       // create deterministic public and private keys based on a seed
-      keys : function(data) {
-        var privateKey = wrapperlib.ethUtil.sha256(data.seed);
-        return {privateKey:privateKey};
+      keys: function (data) {
+        let privateKey = wrapperlib.ethUtil.sha256(data.seed);
+        return {privateKey: privateKey};
+      },
+
+      importKeys: function (data) {
+        return {privateKey: Buffer.from(data.privateKey, 'hex')};
       },
 
       // generate a unique wallet address from a given public key
-      address : function(data) {
-        var publicKey = wrapperlib.ethUtil.privateToPublic(data.privateKey);
-        return '0x'+wrapperlib.ethUtil.publicToAddress(publicKey).toString('hex');
+      address: function (data) {
+        let publicKey = wrapperlib.ethUtil.privateToPublic(data.privateKey);
+        return '0x' + wrapperlib.ethUtil.publicToAddress(publicKey).toString('hex');
       },
 
       // return public key
-      publickey : function(data) {
-        var publicKey = wrapperlib.ethUtil.privateToPublic(data.privateKey);
+      publickey: function (data) {
+        let publicKey = wrapperlib.ethUtil.privateToPublic(data.privateKey);
         return publicKey.toString('hex');
       },
 
       // return private key
-      privatekey : function(data) {
+      privatekey: function (data) {
         return data.privateKey.toString('hex');
       },
 
       // create and sign a transaction
-      transaction : function(data) {
+      transaction: function (data) {
         if (data.mode !== 'token') {
           // Base ETH mode
-          var txParams = {                                               // optional-> data: payloadData
-            nonce: parseLargeIntToHex(data.unspent.nonce),  // nonce
-            gasPrice: parseLargeIntToHex(data.fee/21000),   // we use toString(16) here to specify HEX radix
-            gasLimit: parseLargeIntToHex(21000),            //  but don't use it elsewhere
-            to: data.target,                                // send it to ...
-            value: parseLargeIntToHex(data.amount)          // the amount to send
+          var txParams = { // optional-> data: payloadData
+            nonce: parseLargeIntToHex(data.unspent.nonce), // nonce
+            gasPrice: parseLargeIntToHex(data.fee / 21000), // we use toString(16) here to specify HEX radix
+            gasLimit: parseLargeIntToHex(21000), //  but don't use it elsewhere
+            to: data.target, // send it to ...
+            value: parseLargeIntToHex(data.amount) // the amount to send
           };
         } else {
-          var tokenfeeMultiply = 16;   // [!] must be same as the value in back-end module
+          let tokenfeeMultiply = 16; // [!] must be same as the value in back-end module
           // ERC20-compatible token mode
-          var encoded = encode({ 'func':'transfer(address,uint256):(bool)','vars':['target','amount'],'target':data.target,'amount':parseLargeIntToHex( data.amount ) }); // returns the encoded binary (as a Buffer) data to be sent
+          let encoded = encode({ 'func': 'transfer(address,uint256):(bool)', 'vars': ['target', 'amount'], 'target': data.target, 'amount': parseLargeIntToHex(data.amount) }); // returns the encoded binary (as a Buffer) data to be sent
           var txParams = {
-            nonce: parseLargeIntToHex(data.unspent.nonce),          // nonce
-            gasPrice: parseLargeIntToHex(new Decimal(String(data.fee)).dividedBy(21000*tokenfeeMultiply).times(2).toString()),     // must be 2x normal tx!
-            gasLimit: parseLargeIntToHex(new Decimal(String(21000*tokenfeeMultiply)).dividedBy(2).toString()),                     // should not exceed 300000 !
-            to: data.contract,                                      // send payload to contract address
-            value: '0x0',                                           // set to zero, since we're sending tokens
-            data: encoded                                           // payload as encoded using the smart contract
+            nonce: parseLargeIntToHex(data.unspent.nonce), // nonce
+            gasPrice: parseLargeIntToHex(new Decimal(String(data.fee)).dividedBy(21000 * tokenfeeMultiply).times(2).toString()), // must be 2x normal tx!
+            gasLimit: parseLargeIntToHex(new Decimal(String(21000 * tokenfeeMultiply)).dividedBy(2).toString()), // should not exceed 300000 !
+            to: data.contract, // send payload to contract address
+            value: '0x0', // set to zero, since we're sending tokens
+            data: encoded // payload as encoded using the smart contract
           };
         }
 
         // DEBUG: return JSON.stringify(txParams);
-        
+
         // Transaction is created
-        var tx = new wrapperlib.ethTx(txParams);
+        let tx = new wrapperlib.ethTx(txParams);
 
         // Transaction is signed
         tx.sign(data.keys.privateKey);
-        var serializedTx = tx.serialize();
-        var rawTx = '0x' + serializedTx.toString('hex');
+        let serializedTx = tx.serialize();
+        let rawTx = '0x' + serializedTx.toString('hex');
         return String(rawTx);
       },
       // encode ABI smart contract calls
-      encode : function(data) { return encode(data); }
+      encode: function (data) { return encode(data); }
 
-    }
+    };
     return functions;
   }
 )();
