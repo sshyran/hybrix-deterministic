1cf667a27e8e9cb3641a286afd2c3ea572bc549c
“Marco <marco.alkema@gmail.com>
2019-04-04 14:49:06 -0400

    #40-Fix Bitcoin Cash deterministic.

---
+++ modules/bitcoin_cash/deterministic.js
@@ -0,0 +1,94 @@
+const lib = require('bitcore-lib-cash');
+const cashaddrjs = require('cashaddrjs');
+const bchaddr = require('bchaddrjs');
+
+window.bch = lib;
+window.cashAddr = cashaddrjs;
+
+function mkPrivateKey (seed) {
+  const seedBuffer = Buffer.from(seed, 'utf8');
+  const hash = nacl.to_hex(nacl.crypto_hash_sha256(seedBuffer));
+  const bn = lib.crypto.BN.fromBuffer(hash);
+
+  return new lib.PrivateKey(bn);
+}
+
+function mkAddress (pk) {
+  const address = pk.toAddress();
+  const type = address.type === lib.Address.PayToPublicKeyHash ? 'P2PKH' : 'P2SH';
+  const hash = new Uint8Array(address.hashBuffer);
+
+  return cashaddrjs.encode('bitcoincash', type, hash);
+}
+
+function mkUtxo (utxoData) {
+  return function (utxo, i) {
+    const txData = {
+      address: utxoData.cashAddress,
+      outputIndex: i,
+      script: utxoData.scriptPubKey
+    };
+    return Object.assign(utxo, txData);
+  };
+}
+
+let wrapper = (
+  function () {
+    return {
+      importKeys: data => {
+        console.log('data = ', data);
+        // return {WIF: data.privateKey};
+      },
+
+      // create deterministic public and private keys based on a seed
+      keys: data => {
+        const pk = mkPrivateKey(data.seed);
+
+        return {
+          privateKey: pk
+        };
+      },
+
+      // generate a unique wallet address from a given public key
+      address: data => {
+        return mkAddress(data.privateKey);
+      },
+
+      // return public key
+      publickey: data => {
+        return null;
+      },
+
+      // return private key
+      privatekey: data => {
+        return data.privateKey;
+      },
+
+      transaction: (data, cb, err) => {
+        const targetAddr = data.target;
+        const toAddress = bchaddr.isLegacyAddress(targetAddr) ? bchaddr.toCashAddress(targetAddr) : targetAddr;
+        const utxoUrl = `https://rest.bitcoin.com/v2/address/utxo/${data.source}`;
+
+        fetch(utxoUrl)
+          .then(res => res.json()
+            .then(utxoData => {
+              const utxos = utxoData.utxos
+                .map(mkUtxo(utxoData));
+
+              const transaction = new lib.Transaction()
+                .from(utxos)
+                .to(toAddress, Number(data.amount))
+                .change(data.source)
+                .fee(5430)
+                .sign(data.keys.privateKey);
+
+              cb(transaction.serialize());
+            })
+            .catch(err))
+          .catch(err);
+      }
+    };
+  }
+)();
+
+window.deterministic = wrapper;
